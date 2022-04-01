64f0d605b7ac246f394c9d5f535e56d12805918e
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-04-01 22:01:07 +0530

    Time: 59 ms (13.21%), Space: 18.4 MB (13.89%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    // vector<vector<int>> graphh;
     vector<vector<int>> alll;
     void dfs(int n, vector<int> prnt, int trgt, vector<vector<int>>& graphh){
+        prnt.push_back(n);
         if(n == trgt){
-            prnt.push_back(n);
             alll.push_back(prnt);
             return;
         }
-        prnt.push_back(n);
         for(int i = 0; i < graphh[n].size(); i++){
-            // if(find(prnt.begin(), prnt.end(), graphh[n][i]) == prnt.end()){
             vector<int> temp = prnt;
             dfs(graphh[n][i],temp, trgt, graphh);
-            // }
         }
         return;
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        // graphh = graph;
         dfs(0, {}, graph.size()-1, graph);
         return alll;
     }
