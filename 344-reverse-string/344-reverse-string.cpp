23a0aae5d2b469df992c674ef9a77a50f372e719
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-05-31 09:38:35 +0530

    Time: 42 ms (16.50%), Space: 23 MB (99.92%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        // reverse(s.begin(),s.end());
+        int lo = 0, hi = s.size() - 1;
+        while(hi>=lo){
+            swap(s[lo],s[hi]);
+            hi--;
+            lo++;
+        }
     }
 };
