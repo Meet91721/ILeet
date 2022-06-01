8846d94ff26c66b85dbf75bcd9d4cbd5c458a93a
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-06-01 13:06:41 +0530

    Time: 8 ms (35.39%), Space: 9.6 MB (52.86%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*
         int lo = 0, hi = matrix.size()-1;
         while(hi-lo>1){
             int mid = (hi + lo) / 2;
@@ -37,6 +38,32 @@ public:
         }
         if(matrix[row][lo] == target || matrix[row][hi] == target){
             return true;
+        }
+        return false;
+        */
+        // Thinking as BST
+        int head_y = 0, head_x = matrix[0].size() - 1;
+        while(true){
+            // cout << matrix[head_y][head_x] << '\n';
+            if(matrix[head_y][head_x] == target){
+                return true;
+            }
+            if(matrix[head_y][head_x] > target){
+                if(head_x > 0){
+                    head_x--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(head_y<matrix.size() - 1){
+                    head_y++;
+                }
+                else{
+                    return false;
+                }
+            }
         }
         return false;
     }
