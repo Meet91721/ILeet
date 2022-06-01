c47ea40e25196b8d9eee843daf36f4d1d1af2918
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-06-01 07:24:07 +0530

    Time: 7 ms (28.96%), Space: 8.6 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        // vector<int> res;
-        // res.push_back(nums[0]);
         for(int i = 1; i < nums.size(); i++){
-            // res.push_back(res[i-1]+nums[i]);
             nums[i] = nums[i]+nums[i-1];
         }
         return nums;
