18e696b3ae6ac42a43671d89e740b244facc58af
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-06-06 18:09:36 +0530

    Time: 251 ms (42.38%), Space: 65.8 MB (44.23%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int>trustIn[n+1];
-        set<int>trustsSomeone;
+        unordered_set<int>trustsSomeone;
         
         for(int i = 0; i < trust.size(); i++){
             trustIn[trust[i][1]].push_back(trust[i][0]);
