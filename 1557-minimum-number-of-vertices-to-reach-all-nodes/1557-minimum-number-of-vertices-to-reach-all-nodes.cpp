a536b9d170fa7e2f33c7e99090005f6dd927b30f
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-06-06 18:39:21 +0530

    Time: 317 ms (83.51%), Space: 100.9 MB (57.40%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     
+    /*
+    Sometimes I think too much
     void traversal(vector<bool>&vis, vector<vector<int>>&g, int node){
         vis[node] = 1;
         for(auto nodes: g[node]){
@@ -10,23 +12,17 @@ public:
         }
         return;
     }
+    */
     
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         
-        vector<vector<int>>g(n);
         unordered_set<int>s;
-        
+        vector<int>res;
         for(int i = 0; i < edges.size(); i++){
-            g[edges[i][0]].push_back(edges[i][1]);
             s.insert(edges[i][1]);
         }
-        
-        vector<int>res;
-        vector<bool>vis(n, false);
-        
         for(int i = 0; i < n; i++){
             if(s.find(i) == s.end()){
-                traversal(vis, g, i);
                 res.push_back(i);
             }
         }
