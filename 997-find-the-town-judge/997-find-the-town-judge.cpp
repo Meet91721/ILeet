5d10588aa5322b062cc3371b0102bc6938933b83
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-06-06 18:09:14 +0530

    Time: 396 ms (6.66%), Space: 66.1 MB (43.91%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>trustIn[n+1];
+        set<int>trustsSomeone;
+        
+        for(int i = 0; i < trust.size(); i++){
+            trustIn[trust[i][1]].push_back(trust[i][0]);
+            trustsSomeone.insert(trust[i][0]);
+        }
+        for(int i = 1; i <= n; i++){
+            if(trustIn[i].size() == n - 1){
+                if(trustsSomeone.find(i) == trustsSomeone.end())
+                    return i;
+            }
+        }
+        return -1;
+        
+    }
+};
