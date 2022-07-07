e3bddfcefc4f110c15448913b9849db553949a35
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-07-08 03:33:14 +0530

    Time: 6 ms (61.76%), Space: 6.2 MB (89.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    int dp[101][101][201];
+    int dp[101][101];
     
     bool funcc(string &s1, string &s2, string &s3, int i, int j, int k, string &res){
         if(s3 == res)
             return true;
-        if(dp[i][j][k]!=-1)
-            return dp[i][j][k];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         if(s1[i]==s3[k]){
             res+=s1[i];
             if(funcc(s1,s2,s3,i+1,j,k+1,res))
-                return dp[i][j][k] = true;
+                return dp[i][j] = true;
             res.pop_back();
         }
         if(s2[j]==s3[k]){
             res+=s2[j];
             if(funcc(s1,s2,s3,i,j+1,k+1,res))
-                return dp[i][j][k]=true;
+                return dp[i][j]=true;
             res.pop_back();
         }
-        return dp[i][j][k]=false;
+        return dp[i][j]=false;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
