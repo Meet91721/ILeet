7a70d3e1c336601315fa657fcfef2d46ee6b26eb
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-08-30 08:48:38 +0530

    Time: 1022 ms (21.33%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,7 +1,9 @@
 /* Write your PL/SQL query statement below */
 -- select name as customers from customers minus (select name from customers where id =any (select customerid from orders));
+
 -- (select name as customers from customers) minus
 -- (select name from customers
 -- where id = any (select customerid from orders));
+
 select name as customers from customers
 where id not in (select customerid from orders);
