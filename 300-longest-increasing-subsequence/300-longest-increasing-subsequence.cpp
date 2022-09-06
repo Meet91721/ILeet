3c339313361fd252338ba7f8944f1124337de7ae
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-09-06 15:35:02 +0530

    Time: 1761 ms (8.14%), Space: 43.4 MB (14.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+    
+    int dp[2501][3000];
+    
+    int funcc(int n, vector<int>& nums, int lst){
+        if(nums.size() == n)
+            return 0;
+        int ttt = lst;
+        if(ttt==-1)
+            ttt=2999-1;
+        if(dp[n][ttt]!=-1)
+            return dp[n][ttt];
+        int res = funcc(n+1,nums,lst);
+        if(lst == -1 || nums[lst] < nums[n])
+            res = max(res, 1+funcc(n+1,nums,n));
+        return dp[n][ttt]=res;
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp(nums.size(),1);
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1){
-                    dp[i] = dp[j]+1;
-                }
-            }
-        }
-        return *max_element(dp.begin(), dp.end());
+        memset(dp,-1,sizeof(dp));
+        return funcc(0,nums,-1);
     }
 };
