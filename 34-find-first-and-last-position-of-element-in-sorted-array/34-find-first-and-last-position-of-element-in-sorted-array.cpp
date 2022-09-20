cca087e664811837c6adbf9ceff1eabdc15aaccc
Amin Meet <74113151+Meet91721@users.noreply.github.com>
2022-09-21 03:59:23 +0530

    Time: 12 ms (64.81%), Space: 13.7 MB (18.06%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto it = lower_bound(nums.begin(),nums.end(),target);
-        if(it == nums.end() || *it != target)
+        int one = -1;
+        int high = (int)nums.size()-1, low = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            cout << mid;
+            if(nums[mid] == target){
+                high = mid-1;
+                one = mid;
+            }
+            else if(nums[mid] < target)
+                low = mid+1;
+            else 
+                high = mid-1;
+        }
+        if(one == -1)
             return {-1,-1};
-        auto it1 = upper_bound(nums.begin(),nums.end(),target);
-        
-        advance(it1,-1);
-        int one = distance(nums.begin(),it);
-        int two = distance(nums.begin(),it1);
-        return {one,two};
+        high = (int)nums.size()-1, low = 0;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] <= target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return {one,low-1};
     }
 };
